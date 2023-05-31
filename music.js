055d23d727f3299b68ee58d1360f1a99f49a8a2e
proggenius <133808488+proggenius@users.noreply.github.com>
2023-05-31 13:25:33 +0600

    Add files via upload

---
+++ music.js
@@ -0,0 +1,26 @@
+let trackList = [
+  {
+    name: "A New Beginning",
+    path: "./music/1.mp3",
+    img: "https://cdn.pixabay.com/photo/2017/01/18/17/14/girl-1990347_1280.jpg",
+    singer: "Bensound.com",
+  },
+  {
+    name: "Hey",
+    path: "./music/2.mp3",
+    img: "https://cdn.pixabay.com/photo/2014/05/21/15/18/musician-349790_1280.jpg",
+    singer: "Bensound.com",
+  },
+  {
+    name: "Little Idea",
+    path: "./music/3.mp3",
+    img: "https://cdn.pixabay.com/photo/2015/01/20/13/13/samsung-605439_1280.jpg",
+    singer: "Bensound.com",
+  },
+  {
+    name: "Sunny",
+    path: "./music/4.mp3",
+    img: "https://cdn.pixabay.com/photo/2018/08/27/10/11/radio-cassette-3634616_1280.png",
+    singer: "Bensound.com",
+  },
+];
